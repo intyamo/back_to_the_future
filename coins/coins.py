bae030cf3a2b0bc5485737c5fdac8a92092f1176
Sergey A <sergey.abramenka@gmail.com>
2021-08-26 21:22:02 +0300

    Fix input in circles

--- coins/coins.py
+++ coins/coins.py
@@ -4,13 +4,14 @@
 # Найдите максимальную сумму из любых двух монет.
 # Номинальная стоимость монет: a, b и с тугриков.
 
+
 def max_sum_of_2(a: int, b: int, c: int) -> int:
     pass
 
 
 if __name__ == "__main__":
     a = int(input("a = "))
-    b = int(input("a = "))
-    c = int(input("a = "))
+    b = int(input("b = "))
+    c = int(input("c = "))
 
     print(max_sum_of_2(a, b, c))
